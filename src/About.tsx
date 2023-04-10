5f14ed1e00ff6fe23a9815ea850a3b809162f3aa
louislam <louislam7229@gmail.com>
2023-04-10 15:36:33 -0700

    adjust responsiveness for about page

--- src/About.tsx
+++ src/About.tsx
@@ -15,8 +15,11 @@ const About = () => {
       <section className="about-me-container">
         <div className="about-me">
           <h2>In pursuit of innovation</h2>
-        <p>
-          I pride myself on my attention to detail and ability to find creative solutions to complex problems. In my free time, I enjoy perusing new projects or technology and contributing to open-source projects. I'm excited to continue building innovative software solutions and pushing the boundaries of what's possible.
+          <p>
+            I've loved building things since I was a young kid, from Legos to Rokenboks to Home Depot wood-working workshops on the weekends. However, being a gamer at heart, my actual first encounter with programming was in Diablo II. I taught myself how to set up a bot that used scripts based on an open-source language called AutoIt.
+            </p>
+<p>
+            I pride myself on my attention to detail and ability to find creative solutions to complex problems. In my free time, I enjoy perusing new projects or technology and contributing to open-source projects. I'm excited to continue building innovative software solutions and pushing the boundaries of what's possible.
         </p>
         </div>
         <div className="about-me-pic">
