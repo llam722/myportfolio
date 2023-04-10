52b5c0a0e6b590329bb5760ec51e32ffe2f6f4a4
louislam <louislam7229@gmail.com>
2023-04-09 19:26:00 -0700

    add profile pic and bio

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Nav/>
+        <Nav />
+        <div className='profile-pic'>
+          <a href='home'>
+          <img alt='profile' src='./profilepic.png' />
+          </a>
+        </div>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>
+          Software Engineer, tinkerer, and PC enthusiast.
+        </h2>
         <p>
-          I'm Louis Lam.
+          I'm Louis, a software engineer and entrepreneur based in New York City. I have a passion for contributing to open source products and improving the developer community.
         </p>
       </header>
     </div>
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -7,7 +7,14 @@ const Nav = () => {
     <ul className="Nav-bar">
       <li><a href='about.html'>About</a></li>
       <li><a href='#projects'>Projects</a></li>
-      <li><a href='#contact'>Contact</a></li>
+        <li><a href='#contact'>Contact</a></li>
+         <li><a href='https://www.linkedin.com/in/louis-lam7/' target='_blank' rel="noreferrer">
+          <i className="fa-brands fa-linkedin" aria-hidden='true'></i>
+          <span className='sr-only'>LinkedIn</span></a></li>
+      <li><a href='https://github.com/llam722' target='_blank' rel="noreferrer">
+          <i className="fa-brands fa-github"></i>
+          <span className='sr-only'>Github</span></a></li>
+      <li><a href='./media/Louis-Lam-Resume.pdf' download="" className='button'>Resume</a></li>
     </ul>
   </nav>
   )
