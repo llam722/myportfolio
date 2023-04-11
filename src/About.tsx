7e65d645c3de200bd89fbf71ba3b1ecfd618b115
louislam <louislam7229@gmail.com>
2023-04-11 08:06:28 -0700

    fix text

--- src/About.tsx
+++ src/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
       <section className="about-me-container">
         <div className="about-me">
           <p>
-            I've loved building things since I was a young kid, from Legos to Rokenboks to Home Depot wood-working workshops on the weekends. However, being a gamer at heart, my actual first encounter with programming was in Diablo II. I taught myself how to set up a bot that used scripts based on an open-source language called AutoIt.
+            I've loved building things from a young age, from Legos to Rokenboks to Home Depot wood-working workshops on the weekends. However, being a gamer at heart, my actual first encounter with programming was in Diablo II. I taught myself how to set up a bot that used scripts based on an open-source language called AutoIt.
           </p>
           <p>
             As I grew older, I realized that working on cars and custom PCs involved more than just hands-on mechanical skills - they also required coding skills to program (ECU tuning) and optimize various features. This led me to deepen my knowledge of coding and pursue further programming projects in my free time. Today, as a software engineer, I leverage my passion for building and problem-solving to create innovative software solutions that push the boundaries of what's possible. This pivotal experience ignited a passion for programming within me, driving me to hone my skills and pursue a career as a software engineer. Today, I thrive on leveraging my extensive knowledge and problem-solving abilities to create innovative software solutions that push the boundaries of what's possible.
