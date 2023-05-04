8bb71cb5fa4fbbc808ab0764e91c8f58e0ccc056
louislam <louislam7229@gmail.com>
2023-05-03 17:35:36 -0700

    adjust contact

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -3,17 +3,15 @@ import './Contact.css'
 const Contact = () => {
   return (
       <div className="Contact">
-        <div className="header">
-            <h1>Collaborate to Innovate – </h1>
-        </div>
+            <h1>Collaborate to Innovate </h1>
         <section className="connect-container">
+          <div className="connect-pic">
+            <img alt='cat laptop' src="./cat-laptop.jpg" />
+          </div>
           <div className="connect">
             <p>
               I'm passionate about working with like-minded creatives and entrepreneurs to bring new ideas to life. Whether you have a project in mind or just want to connect and share your passions, I'm always eager to collaborate and learn something new. Let's take it to the next level!
               </p>
-          </div>
-          <div className="connect-pic">
-            <img alt='cat laptop' src="./cat-laptop.jpg" />
           </div>
         </section>
       </div>
