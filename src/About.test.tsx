c76c36229c0aa975cc30ff73a9f039cb26b15243
louislam <louislam7229@gmail.com>
2023-05-23 16:42:29 -0700

    remove import to allow testing coverage

--- src/About.test.tsx
+++ src/About.test.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import About from './About';
-import 'testing-@testing-library/jest-dom'
+
 
 
 describe('About', () => {
   it('renders a paragraph', () => {
     render(<About />);
 
-    const heading = screen.getByRole('heading', {
-      name: /from legos to code: a journey of building and problem-solving/i,
-    })
+    // const heading = screen.getByRole('heading', {
+    //   name: /From Legos to Code: A Journey of Building and Problem-Solving/i,
+    // })
+    const heading = screen.getByText(/From Legos to Code: A Journey of Building and Problem-Solving/i)
+
     expect(heading).toBeInTheDocument();
   })
 })
