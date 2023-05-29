9039e7dd258aac077d35c9d306ccaee5cecf3006
louislam <louislam7229@gmail.com>
2023-05-28 22:52:33 -0700

    add test for Projects

--- src/Projects.test.tsx
+++ src/Projects.test.tsx
@@ -1,18 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import Projects from './Projects';
 
 
 
-describe('Home', () => {
+describe('Projects', () => {
   it('should render a heading', () => {
-    render(<Home />);
+    render(<Projects />);
     const heading = screen.getByText(/tinkerer/i)
 
     expect(heading).toBeInTheDocument();
   })
 
   it('should render an image', () => {
-    render(<Home />);
+    render(<Projects />);
     
   const image = screen.getAllByRole('img')
   expect(image).toHaveLength(5);
