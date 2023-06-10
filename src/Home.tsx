c6e0efd27076add562bd77200ea70d979fcaccb9
louislam <louislam7229@gmail.com>
2023-06-09 17:32:23 -0700

    serve Home images from source

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,10 +15,10 @@ const Home = () => {
       </section>
         <div className="picture-div-container">
       <div className='picture-div'>
-        <img alt='notion event' src='https://res.cloudinary.com/djnwvbzzr/image/upload/v1686353065/Portfolio/Notion-event_e6rspx.jpg'></img>
-        <img alt='laptop coffee' src='https://res.cloudinary.com/djnwvbzzr/image/upload/v1686353074/Portfolio/laptop-coffee_zvejik.jpg'></img>
-        <img alt='watercooled pc' src='https://res.cloudinary.com/djnwvbzzr/image/upload/v1686353069/Portfolio/watercooling_jvq0jm.jpg'></img>
-        <img alt='notion sticker' src='https://res.cloudinary.com/djnwvbzzr/image/upload/v1686353073/Portfolio/notion-sticker_g4s9g8.jpg'></img>
+        <img alt='notion event' src='./Notion-event.jpg'></img>
+        <img alt='laptop coffee' src='./laptop-coffee.jpg'></img>
+        <img alt='watercooled pc' src='./watercooling.jpg'></img>
+        <img alt='notion sticker' src='./notion-sticker.jpg'></img>
         </div>
         </div>
         <Projects/>
