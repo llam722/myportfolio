11862dcc1502707792d30d2dfb41cabf52ae211e
louislam <louislam7229@gmail.com>
2024-02-02 18:24:45 -0800

    feat(portfolio): remove projects from home and create project on navbar

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -15,6 +15,9 @@ const Nav = () => {
         <li><Link to='/about'>
         About
         </Link></li>
+        <li><Link to='/projects'>
+        Projects
+        </Link></li>
         <li><Link to='/contact'>
         Contact
         </Link></li>
