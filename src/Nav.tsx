00fe59522010c0bdf2bf8ae0d2d7909dd37c3ed8
louislam <louislam7229@gmail.com>
2024-02-03 18:41:58 -0800

    fix(navbar): adjust linkedin and github icons to text

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,37 +1,47 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import './Nav.css'
+import "./Nav.css";
 
 const Nav = () => {
   return (
     <nav>
-      
       <ul className="Nav-bar">
         <li>
-          <Link to='/' className='profile-pic'>
-          <img alt='profile' src='./profilepic.png' />
+          <Link to="/" className="profile-pic">
+            <img alt="profile" src="./profilepic.png" />
           </Link>
         </li>
-        <li><Link to='/about'>
-        About
-        </Link></li>
-        <li><Link to='/projects'>
-        Projects
-        </Link></li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
         {/* <li><Link to='/contact'>
         Contact
         </Link></li> */}
-         <li><a href='https://www.linkedin.com/in/louis-lam7/' target='_blank' rel="noreferrer">
-          <i className="fa-brands fa-linkedin" aria-hidden='true'></i>
-          <span className='sr-only'>LinkedIn</span></a></li>
-      <li><a href='https://github.com/llam722' target='_blank' rel="noreferrer">
-          <i className="fa-brands fa-github"></i>
-          <span className='sr-only'>Github</span></a></li>
-      {/* <li><a href='./Louis_Lam_Resume.pdf' download="" className='button'>Resume</a></li> */}
-    
-    </ul>
-      </nav>
-  )
-}
+        <li>
+          <a
+            href="https://www.linkedin.com/in/louis-lam7/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {/* <i className="fa-brands fa-linkedin" aria-hidden='true'></i>
+          <span className='sr-only'>LinkedIn</span> */}
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/llam722" target="_blank" rel="noreferrer">
+            {/* <i className="fa-brands fa-github"></i>
+            <span className="sr-only">Github</span> */}
+            Github
+          </a>
+        </li>
+        {/* <li><a href='./Louis_Lam_Resume.pdf' download="" className='button'>Resume</a></li> */}
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
