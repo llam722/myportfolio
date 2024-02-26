274004e74b3b21014dd3730caea8cfab28b99737
louislam <louislam7229@gmail.com>
2024-02-26 00:29:25 -0500

    feat(analytics): add posthog and restore google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import posthog from "posthog-js";
 
+posthog.init("phc_8vMUx3Xx921fHLugGSog2PdEbmq4D8r3sUEtdcIo8uP", {
+  api_host: "https://app.posthog.com",
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </React.StrictMode>
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
